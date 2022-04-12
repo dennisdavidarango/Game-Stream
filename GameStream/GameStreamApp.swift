6b10052253fed2b580063df245dbcab6cabacd46
Dennis <dhavid-arango77@hotmail.com>
2022-04-12 18:38:58 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Dennis David Arango Mendieta on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
